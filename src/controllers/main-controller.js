965fc155923f4a73e1b6b4566c293abbbddda06e
Alejandro <dupuyalejandro04@gmail.com>
2024-02-05 11:10:15 +1300

    create structure and design

--- src/controllers/main-controller.js
+++ src/controllers/main-controller.js
@@ -2,6 +2,15 @@ const mainController = {
   index: (req, res) => {
     res.render("index");
   },
+  aboutus: (req, res) => {
+    res.render("aboutus");
+  },
+  contact: (req, res) => {
+    res.render("contact");
+  },
+  login: (req, res) => {
+    res.render("login");
+  },
 };
 
 module.exports = mainController;
--- src/routers/main.js
+++ src/routers/main.js
@@ -3,5 +3,8 @@ const router = Router();
 const mainController = require("../controllers/main-controller.js");
 
 router.get("/", mainController.index);
+router.get("/aboutus", mainController.aboutus);
+router.get("/contact", mainController.contact);
+router.get("/login", mainController.login);
 
 module.exports = router;
